211d7d0e664d4844843152da54d1866e20d62a3f
Swayam-thakur <thakurswayam98@gmail.com>
2026-03-09 10:24:44 +0530

    Add files via upload

---
+++ 1moisture_code.ino
@@ -0,0 +1,29 @@
+#include <DHT.h>
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+
+  Serial.begin(115200);
+  dht.begin();
+
+}
+
+void loop() {
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  Serial.print("Temp: ");
+  Serial.print(temperature);
+  Serial.print(" °C   ");
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  delay(2000);
+}
